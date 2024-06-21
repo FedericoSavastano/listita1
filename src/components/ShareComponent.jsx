6772e6a2b259f5b6eaa4140bb8096b56dc5b5b8a
FedericoSavastano <f.savastano@hotmail.com>
2024-06-20 21:15:18 -0300

    shares

--- src/components/ShareComponent.jsx
+++ src/components/ShareComponent.jsx
@@ -8,27 +8,57 @@ const ShareComponent = () => {
 
     const handleShare = async () => {
         const data = localStorage.getItem('listElements');
-        const encodedData = JSON.stringify(data);
+        const encodedData = encodeURIComponent(JSON.stringify(data));
         const baseUrl = window.location.href;
         const shareableUrl = `${baseUrl}?data=${encodedData}`;
 
-        try {
-            const response = await axios.post(
-                'https://api.rebrandly.com/v1/links',
+        console.log(shareableUrl);
 
-                {
-                    destination: shareableUrl,
-                },
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'apikey': '1ca5562359c6422783f32afa84fa8549',
-                    },
-                }
-            );
-            console.log(response.data.shortUrl);
-        } catch (err) {
-            console.log('Error shortening the URL: ' + err.message);
+        // try {
+        //     const response = await axios.post(
+        //         'https://api.tinyurl.com/create',
+        //         {
+        //             url: encodeURIComponent(shareableUrl),
+        //             domain: 'tiny.one',
+        //             description: 'string',
+        //         },
+        //         {
+        //             headers: {
+        //                 Authorization:
+        //                     'Bearer {ZkMUOL2StHNJ76YHq3qhkWSz1gJi0BKbTF3m5tu5UmW5R6NOZFwexygKQCS7}', // Replace with your TinyURL API key
+        //                 'Content-Type': 'application/json',
+        //             },
+        //         }
+        //     );
+        //     console.log(response.data.data.tiny_url);
+        //     setShortUrl(response.data.data.tiny_url);
+        // } catch (error) {
+        //     console.error('Error creating short URL', error);
+        // }
+
+        const url = 'https://url-shortener42.p.rapidapi.com/shorten/';
+        const options = {
+            method: 'POST',
+            headers: {
+                'x-rapidapi-key':
+                    '0a00b49b3dmsh470ed428ca94fb5p1ee061jsne49e6e95f9d9',
+                'x-rapidapi-host': 'url-shortener42.p.rapidapi.com',
+                'Content-Type': 'application/json',
+            },
+            body: {
+                url: shareableUrl,
+                validity_duration: 5,
+            },
+        };
+
+        try {
+            const response = await fetch(url, options);
+            const result = await response.text();
+            console.log(result);
+
+            setShortUrl(result);
+        } catch (error) {
+            console.error(error);
         }
 
         // You can use navigator.share if you want to use the Web Share API
@@ -36,13 +66,13 @@ const ShareComponent = () => {
             navigator
                 .share({
                     title: 'Share Data',
-                    url: shareableUrl,
+                    url: shortUrl,
                 })
                 .catch((error) => console.error('Error sharing', error));
         } else {
             // Fallback for browsers that don't support the Web Share API
             navigator.clipboard
-                .writeText(shareableUrl)
+                .writeText(shortUrl)
                 .then(() => alert('Link copied to clipboard'))
                 .catch((error) => console.error('Error copying link', error));
         }
